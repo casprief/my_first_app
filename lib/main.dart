e0ac1d2d77360e0ae48290b0c3de80aedc05e036
casprief <felipe.castaneda@eu4m.eu>
2021-01-17 17:48:12 +0100

    Implement Column

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,31 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.deepOrange,
       ),
-      body: Row(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text('Widget 1'),
-          FlatButton(
-              onPressed: (){},
-              child: Text('Widget 2'),
-              color: Colors.amber,
+        children: [
+          Row(
+            children: [
+              Text('Hello '),
+              Text('World'),
+            ],
           ),
           Container(
-            color: Colors.deepOrange,
+            padding: EdgeInsets.all(20.0),
+            color: Colors.lightBlue,
+            child: Text('one'),
+          ),
+          Container(
             padding: EdgeInsets.all(30.0),
-            child: Text('Widget 3'),
-          )
+            color: Colors.blueAccent,
+            child: Text('two'),
+          ),
+          Container(
+            padding: EdgeInsets.all(40.0),
+            color: Colors.deepPurple,
+            child: Text('three'),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
